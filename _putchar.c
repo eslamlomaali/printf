60ce70fbce776dd61883fe9d79b386f25c2f406f
eslam <eslam@gmail.com>
2023-08-18 04:19:19 -0700

    done

--- _putchar.c
+++ _putchar.c
@@ -11,3 +11,16 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+/**
+ * _puts - prints a string to stdout
+ * @string: pointer to the string to print
+ * Return: number of chars written
+ */
+int _puts(char *string)
+{
+	register short i;
+
+	for (i = 0; string[i]; i++)
+		_putchar(string[i]);
+	return (i);
+}
