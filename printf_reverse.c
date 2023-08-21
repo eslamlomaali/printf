29b3d641590f1d741907daa2e4049850b3836154
kali2105 <anassalaoui0593@gmail.com>
2023-08-21 16:49:49 -0700

    debugging_reversing_printf

--- printf_reverse.c
+++ printf_reverse.c
@@ -8,15 +8,17 @@
 
 int printf_reverse(va_list l)
 {
-	int x = 0, z;
+	int z, x = 0;
 	char *c = va_arg(l, char *);
 
 	if (!c)
 		c = "(null)";
 
-	while (c[x])
-		x++;
+	/* Calculate the x of the c */
+	for (x = 0; c[x] != '\0'; x++)
+		;
 
+	/* Print the c in reverse order */
 	for (z = x - 1; z >= 0; z--)
 		_putchar(c[z]);
 
