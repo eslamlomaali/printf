7b6c8c4ea7643d409fb26f9c2ed9a5f321e979a0
kali2105 <anassalaoui0593@gmail.com>
2023-08-21 17:36:45 -0700

    Fixing_pointer_buffer_hundler

--- printf_pointer.c
+++ printf_pointer.c
@@ -1,18 +1,18 @@
 #include "main.h"
-
 /**
- * printf_pointer - prints an hexgecimal number.
- * @value: arguments.
+ * printf_pointer - prints an hexadecimal representation of a pointer.
+ * @value: argument.
  * Return: counter.
  */
-
 int printf_pointer(va_list value)
 {
 	void *p;
 	char *c = "(nil)";
-	long int a;
-	int y;
-	int z;
+	unsigned long int num_addrs;
+	char map_to[] = "0123456789abcdef";
+	int z, length;
+	char hex_buffer[50];
+	int ind = sizeof(hex_buffer) - 1;
 
 	p = va_arg(value, void*);
 	if (p == NULL)
@@ -24,9 +24,24 @@ int printf_pointer(va_list value)
 		return (z);
 	}
 
-	a = (unsigned long int)p;
+	num_addrs = (unsigned long int)p;
+
 	_putchar('0');
 	_putchar('x');
-	y = printf_hex(a);
-	return (y + 2);
+
+	hex_buffer[ind] = '\0';
+
+	while (num_addrs > 0)
+	{
+		hex_buffer[--ind] = map_to[num_addrs % 16];
+		num_addrs /= 16;
+	}
+
+	length = sizeof(hex_buffer) - ind - 1;
+	for (; ind < (int)sizeof(hex_buffer) - 1; ind++)
+	{
+		_putchar(hex_buffer[ind]);
+	}
+
+	return (length + 2);
 }
